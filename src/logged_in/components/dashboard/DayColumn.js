c0e351a27da07013dce0c8bd3f748e49fd1d6ffa
Amil Naik <amil.naik00@gmail.com>
2023-02-11 16:41:17 -0800

    Hover on unselected time slots

--- src/logged_in/components/dashboard/DayColumn.js
+++ src/logged_in/components/dashboard/DayColumn.js
@@ -1,7 +1,8 @@
-import React, {Fragment } from 'react'
+import React, { Fragment } from 'react'
 
-const DayColumn = ({ day, isMouseDown }) => {
-  const handleMouseOver = (event) => {
+const DayColumn = ({ day, isMouseDown }) => {  
+
+  const handleMouseInside = (event) => {
     if (isMouseDown) {
       if (event.currentTarget.classList.contains("tdSelected")) {
         event.currentTarget.classList.remove("tdSelected")
@@ -9,19 +10,20 @@ const DayColumn = ({ day, isMouseDown }) => {
 
       else {
         event.currentTarget.classList.add("tdSelected")
+        event.currentTarget.classList.remove("tdUnselectedHover")
       }
+    }
 
+    else if(event.currentTarget.classList.contains("tdUnselectedHover")){
+      event.currentTarget.classList.remove("tdUnselectedHover")
     }
   }
 
-  const handleClick = (event) => {
-    if (event.currentTarget.classList.contains("tdSelected")) {
-      event.currentTarget.classList.remove("tdSelected")
+  const handleUnselectedHover = (event) => {
+    if (!event.currentTarget.classList.contains("tdSelected")) {
+      event.currentTarget.classList.add("tdUnselectedHover")
     }
 
-    else {
-      event.currentTarget.classList.add("tdSelected")
-    }
   }
 
   return (
@@ -35,18 +37,18 @@ const DayColumn = ({ day, isMouseDown }) => {
                 </tr>
               </thead>
               <tbody>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
-                <tr><td onMouseOver={handleMouseOver} onClick={handleClick}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
+                <tr><td onMouseLeave={handleMouseInside} onMouseEnter={handleUnselectedHover}></td></tr>
               </tbody>
           </table>
           </Fragment>
